b2a89d6c2063e35da8462db9af30c9e82c1e6c63
thedivtagguy <amanbhargava2001@gmail.com>
2025-02-12 15:13:13 +0530

    chore: initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+</style>
