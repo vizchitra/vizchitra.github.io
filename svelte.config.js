bb8ecc9baac4e6cdbe116bcce8d2998154431e70
thedivtagguy <amanbhargava2001@gmail.com>
2025-02-19 21:04:06 +0530

    chore: remove base path

--- svelte.config.js
+++ svelte.config.js
@@ -11,9 +11,7 @@ const config = {
 			precompress: false,
 			strict: true
 		}),
-		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/site' : ''
-		},
+
 		appDir: 'app'
 	},
 	preprocess: vitePreprocess()
