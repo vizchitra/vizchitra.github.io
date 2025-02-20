b2d8566b65b9306d3679ea82360148e16846ce74
thedivtagguy <amanbhargava2001@gmail.com>
2025-02-20 19:01:46 +0530

    fix: fixes direct linking, closes #55

--- src/lib/components/PageHeader.svelte
+++ src/lib/components/PageHeader.svelte
@@ -3,7 +3,7 @@
 	export let title: string;
 </script>
 
-<div class="relative h-48">
+<div class="relative mt-20 h-48">
 	<BannerPolygon staticBanner={true} />
 	<div class="absolute inset-0 flex items-center justify-center">
 		<h1 class="font-display rounded-2xl bg-white px-4 text-[2rem] font-bold">{title}</h1>
