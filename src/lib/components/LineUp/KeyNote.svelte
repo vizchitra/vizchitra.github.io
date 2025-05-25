478836baa444c75e133a7b372def079cf15cd88c
urmilaj <urmila.j@hotmail.com>
2025-05-25 23:14:04 +0530

    fix keynote speaker design

--- src/lib/components/LineUp/KeyNote.svelte
+++ src/lib/components/LineUp/KeyNote.svelte
@@ -10,38 +10,40 @@
 			title_1: 'The Data Is Here.',
 			title_2: 'But Whom Is It Serving?',
 			title_color: 'text-viz-pink-dark',
+			keynote_time: 'Opening keynote',
 			time: '27 June | 09:30 - 10:00 am'
 		},
 		{
 			image: anand,
 			alt: 'Anand',
 			speaker: 'S ANAND',
-			job_description: `LLM Psychologist @ Straive
+			job_description: `LLM Psychologist @ Straive and
 			CEO @ Gramener`,
 			title_1: 'Data Design By Dialogue.',
 			title_2: 'Visualising Data with LLMs',
 			title_color: 'text-viz-orange-dark',
+			keynote_time: 'Closing keynote',
 			time: '27 June | 04:15 - 04:45 am'
 		}
 	];
 </script>
 
-<div class="grid grid-cols-1 gap-4 py-8 lg:grid-cols-2">
+<div class="grid grid-cols-1 gap-4 lg:grid-cols-2">
 	{#each speakerInfo as speaker}
-		<div class="grid grid-cols-2 items-stretch gap-2">
-			<div>
+		<div class="grid grid-cols-3 self-center gap-2">
+			<div class="col-span-1">
 				<img src={speaker.image} alt={speaker.alt} />
 			</div>
-			<div>
-				<h3>
-					<i class="text-viz-grey font-[Cairo] text-2xl font-bold">{speaker.speaker}</i>
+			<div class="col-span-2">
+				<h3 class="text-viz-grey font-[Cairo]">
+					{speaker.speaker}
 				</h3>
 				<span class="text-sm">{speaker.job_description}</span>
-				<h3 class="{speaker.title_color} font-[Cairo] text-lg">
-					<span class="font-bold">{speaker.title_1}</span><br />
+				<h3 class="font-[Cairo] font-semibold">
+					<span>{speaker.title_1}</span><br />
 					<span>{speaker.title_2}</span>
 				</h3>
-				<p class="p-0 text-xs">{speaker.time}</p>
+				<p class="p-0 text-xs">{speaker.keynote_time}</p>
 			</div>
 		</div>
 	{/each}
