7253c58482da0050c92ce8d48a48bc54b659c894
urmilaj <urmila.j@hotmail.com>
2025-05-25 23:43:48 +0530

    fix text alignment

--- src/lib/components/LineUp/Speakers2025.svelte
+++ src/lib/components/LineUp/Speakers2025.svelte
@@ -1,6 +1,6 @@
 <div class="flex flex-col py-4 bg-viz-grey/5 mt-4 p-4">
     <span class="text-viz-pink-dark text-center font-[Cairo] text-xl"> COMING SOON !</span>
-    <span class="text-left font-[Cairo] px-12 text-lg"
+    <span class="text-justify font-[Cairo] px-10 text-lg"
         >As we confirm our speakers and schedule, stay tuned to find all the goodness we have
         packed in just for you.
     </span>
