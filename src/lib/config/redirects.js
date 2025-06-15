f15598f840445882f33b90c9821b631848af731c
Amit Kapoor <amitkaps@gmail.com>
2025-06-15 18:39:16 +0530

    Update redirects.js (#99)
    Fixes #98

--- src/lib/config/redirects.js
+++ src/lib/config/redirects.js
@@ -4,5 +4,9 @@
 export const redirects = {
 	friends: 'https://chat.whatsapp.com/CbIu7z6ITmGFvwfw0BjDdL',
 	conference: 'https://hasgeek.com/VizChitra/2025',
-	cfp: 'https://hasgeek.com/VizChitra/2025/sub'
+	cfp: 'https://hasgeek.com/VizChitra/2025/sub',
+        schedule: 'https://hasgeek.com/VizChitra/2025/schedule',
+        feedback: 'https://vizchitra.com',
+	tickets: 'https://hasgeek.com/VizChitra/2025/#tickets'
+	
 };
