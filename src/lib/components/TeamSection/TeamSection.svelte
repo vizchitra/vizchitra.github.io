21d2e857b4115bb3f0d5b3215a26159083260fe5
schubert-da <schubertdeabreu2@gmail.com>
2025-06-17 23:38:23 +0530

    add socials to teams page

--- src/lib/components/TeamSection/TeamSection.svelte
+++ src/lib/components/TeamSection/TeamSection.svelte
@@ -4,17 +4,63 @@
 	import { onMount } from 'svelte';
 	import MemberPentagon from './MemberPentagon.svelte';
 
+	import instagram from '$lib/assets/images/socials/instagram.svg';
+	import twitter from '$lib/assets/images/socials/twitter.svg';
+	import linkedIn from '$lib/assets/images/socials/linkedin.svg';
+	import blueSky from '$lib/assets/images/socials/bluesky.svg';
+	import youtube from '$lib/assets/images/socials/youtube.svg';
+	import github from '$lib/assets/images/socials/github.svg';
+
 	let teamData = [];
 
 	onMount(async () => {
 		teamData = await csv(`${base}/data/vizchitra_team.csv`);
 	});
+
+	const socials = [
+		{
+			name: 'instagram',
+			icon: instagram,
+			alt: 'instagram link'
+		},
+		{
+			name: 'twitter',
+			icon: twitter,
+			alt: 'twitter link'
+		},
+		{
+			name: 'bluesky',
+			icon: blueSky,
+			alt: 'bluesky link'
+		},
+		{
+			name: 'linkedin',
+			icon: linkedIn,
+			alt: 'linkedin link'
+		}
+	];
 </script>
 
 {#if teamData}
 	<div class="team-section flex flex-wrap justify-center gap-5 md:min-w-[420px]">
 		{#each teamData.sort((a, b) => a.order - b.order) as teamMember}
-			<MemberPentagon memberData={teamMember} />
+			<div class="team-member mb-4 flex flex-col items-center gap-2">
+				<MemberPentagon memberData={teamMember} />
+
+				<div class="socials-row mt-auto flex flex-row gap-3">
+					{#each socials as social}
+						{#if teamMember[social.name]}
+							<a href={teamMember[social.name]} target="_blank">
+								<img
+									src={social.icon}
+									alt={social.alt}
+									class="h-5 w-5 opacity-70 transition-all duration-50 hover:[filter:invert(33%)_sepia(96%)_saturate(1231%)_hue-rotate(308deg)_brightness(99%)_contrast(99%)] md:h-6 md:w-6"
+								/>
+							</a>
+						{/if}
+					{/each}
+				</div>
+			</div>
 		{/each}
 	</div>
 {/if}
