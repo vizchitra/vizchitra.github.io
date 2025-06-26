1b2c5132721f99d0a21c0ea110997de64301e420
Amit Kapoor <amitkaps@gmail.com>
2025-06-26 16:21:16 +0530

    Update redirects.js

--- src/lib/config/redirects.js
+++ src/lib/config/redirects.js
@@ -5,9 +5,10 @@ export const redirects = {
 	friends: 'https://chat.whatsapp.com/CbIu7z6ITmGFvwfw0BjDdL',
 	conference: 'https://hasgeek.com/VizChitra/2025',
 	cfp: 'https://hasgeek.com/VizChitra/2025/sub',
-        schedule: 'https://hasgeek.com/VizChitra/2025/schedule',
-        feedback: ' https://forms.gle/ai8npdr4GKHDHkZw5',
+    schedule: 'https://hasgeek.com/VizChitra/2025/schedule',
+    feedback: 'https://forms.gle/ai8npdr4GKHDHkZw5',
 	tickets: 'https://hasgeek.com/VizChitra/2025/#tickets',
 	flash: 'https://forms.gle/c62J2Pttascvy37W9',
+    workfeedack: 'https://forms.gle/qw5xB57QBh1h6P4Q9',
 	merchandise: 'https://hasgeek.com/VizChitra/merchandise/' 
 };
