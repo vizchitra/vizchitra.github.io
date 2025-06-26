d507466512c50516f151ff63b1ce3326f47f9c49
Amit Kapoor <amitkaps@gmail.com>
2025-06-26 22:10:14 +0530

    Update redirects.js (#113)

--- src/lib/config/redirects.js
+++ src/lib/config/redirects.js
@@ -12,6 +12,7 @@ export const redirects = {
 	workfeedback: 'https://forms.gle/qw5xB57QBh1h6P4Q9',
 	merchandise: 'https://hasgeek.com/VizChitra/merchandise/',
 	map: 'https://maps.app.goo.gl/Xa6N8q7s5S2Tm1559',
-	'sponsor-feedback': 'https://forms.gle/4TBRt6zTuMdMMiyKA'
+	'sponsor-feedback': 'https://forms.gle/4TBRt6zTuMdMMiyKA',
+	guide: 'https://drive.usercontent.google.com/download?id=1g3e1RkHpv7pW9SZCR4m9FME8rKJ3lSo_&authuser=0&acrobatPromotionSource=GoogleDriveListView'
 	
 };
