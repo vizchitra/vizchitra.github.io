d9e1f2409cb7e5a0dbc43b088b1ebec064e10edc
Amit Kapoor <amitkaps@gmail.com>
2025-12-30 15:33:13 +0530

    refactor(structure): move PageHeader to structure/header; export from structure; remove duplicated layout components; add compatibility re-exports

--- src/lib/components/structure/header/index.ts
+++ src/lib/components/structure/header/index.ts
@@ -1,2 +1,3 @@
 export { default as NavMenu } from './NavMenu.svelte';
 export { default as MobileNavDrawer } from './MobileNavDrawer.svelte';
+export { default as PageHeader } from './PageHeader.svelte';
